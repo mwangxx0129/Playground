3b961de75569046001627233fc78450a801efbdf
mwangxx0129 <mwangxx@gmail.com>
2018-05-18 09:20:37 -0700

    add: convert HHmm to MM

--- src/SpareTime.java
+++ src/SpareTime.java
@@ -5,6 +5,18 @@ import java.util.List;
 public class SpareTime {
     public static void main(String[] args) {
         System.out.println("hello");
+        testConvert();
+    }
+
+    public static void testConvert() {
+        for (int i = 0; i < 1000; ++ i) {
+            String time = MMToHHmm(i);
+            int test = HHmmToMinute(time);
+            System.out.println(i + "\t" + time + "\t" + test);
+        }
+    }
+
+    public static void test() {
         int[][] busyTimes = {
                 {1,4},
                 {2,5},
@@ -15,13 +27,12 @@ public class SpareTime {
             System.out.println(e[0] + "\t" + e[1]);
         }
     }
+
     public static List<int[]> getSpareTime(int[][] busyTimes) {
         Arrays.sort(busyTimes, (a, b) -> a[0] - b[0]);
         List<int[]> result = new ArrayList<>();
         int last = busyTimes[0][1];
         for (int i = 1; i < busyTimes.length; ++ i) {
-
-
             if (last >= busyTimes[i][0]) {
                 last = Math.max(last, busyTimes[i][1]);
             } else {
@@ -31,5 +42,17 @@ public class SpareTime {
 
         }
         return result;
+    }
+
+    // 10:00
+    static int HHmmToMinute(String time) {
+        String[] tokens = time.split(":");
+        return Integer.valueOf(tokens[0]) * 60  + Integer.valueOf(tokens[1]);
+    }
+
+    static String MMToHHmm(int minute) {
+        int h = minute / 60;
+        int m = minute % 60;
+        return "" + (h >= 10 ? h : "0" + h) + ":" + (m >= 10 ? m : "0" + m);
     }
 }
