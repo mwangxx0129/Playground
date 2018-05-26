8c2e1aaf8b641b71f31675a62b6411883ef02fa9
Xinxin Wang <xinxin@MacBook-Pro-2.local>
2018-05-25 18:24:08 -0700

    remove dirty

--- src/May25_2018/StealCake.java
+++ src/May25_2018/StealCake.java
@@ -67,9 +67,6 @@ public class StealCake {
             }
             dp[weight] = max;
         }
-        for (int i = 0; i < dp.length; ++ i) {
-            System.out.println(i + "\t" + dp[i]);
-        }
         return dp[maxWeight];
     }
 }
