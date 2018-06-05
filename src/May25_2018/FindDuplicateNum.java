d5e75704810e3d64cc80d1a9fee4d758aa2e4eb1
mwangxx0129 <mwangxx@gmail.com>
2018-06-04 18:07:16 -0700

    add: find dup & multiple recursively

---
+++ src/May25_2018/FindDuplicateNum.java
@@ -0,0 +1,27 @@
+package May25_2018;
+
+public class FindDuplicateNum {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        int [][] nums = {{3,2,4,2,1}, {2,2,2,2}, {1,2,1}};
+
+        int result = solution(nums[2]);
+        System.out.println(result);
+    }
+    public static int solution(int[] nums) {
+        int s = 0, f = 0;
+        int n = nums.length;
+        while (s < n && f < n) {
+            s = nums[s];
+            f = nums[f];
+            f = nums[f];
+            if (s == f) break;
+        }
+        f = 0;
+        while (s < n && s != f) {
+            s = nums[s];
+            f = nums[f];
+        }
+        return s;
+    }
+}
---
+++ src/May25_2018/MultipleRecursive.java
@@ -0,0 +1,36 @@
+package May25_2018;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MultipleRecursive {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        int[][] inputs = {{1,6,8,4,9,13,12},{1,2,3,2}};
+
+        int[] result = solution(inputs[1]);
+        System.out.println(Arrays.toString(result));
+    }
+
+    public static int[] solution(int[] nums) {
+        int [] result = new int[nums.length];
+        Map<Integer, Integer> h = new HashMap<>();
+        for (int e : nums) {
+            h.put(e, -1);
+        }
+        for (int i = 0; i < nums.length; ++ i) {
+            result[i] = calculateRecursive(h, nums[i], 2);
+        }
+
+        return result;
+    }
+
+    public static int calculateRecursive(Map<Integer, Integer> h, int e, int f) {
+        if (h.containsKey(f * e)) {
+            h.put(e, calculateRecursive(h, 2 * e, f));
+            return h.get(e);
+        }
+        return f * e;
+    }
+}
