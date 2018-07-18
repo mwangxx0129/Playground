c0e93e1c6c405a2145239f194f115a454763c8e3
mwangxx0129 <mwangxx@gmail.com>
2018-07-17 22:37:31 -0400

    save dialog to backend

--- socket.io/examples/chat/index.js
+++ socket.io/examples/chat/index.js
@@ -21,13 +21,13 @@ chatRooms = {
 }
 */
 var chatRooms = {};
+var dialogInfo = {};
 
 var numUsers = 0;
 
 io.on('connection', (socket) => {
   var addedUser = false;
   
-
   // when the client emits 'new message', this listens and executes
   socket.on('new message', (data) => {
     // we tell the client to execute 'new message'
@@ -35,12 +35,20 @@ io.on('connection', (socket) => {
       username: socket.username,
       message: data
     });
-
+    dialogInfo[socket.id]['context'].push({
+      "value": data,
+      "role": socket.userInfo.role,
+      "worker_id": socket.id,
+      "timestamp": Date.now(),
+      "terminal": false
+    });
+    console.log(socket.id + " -> "+ socket.roomId + ": " + data);
   });
 
   // new room
   socket.on('new room', (userInfo) => {
-
+    socket.userInfo = userInfo;
+    
     var topic = userInfo['topic'];
     var roomId = socket.id;
     if (!(topic in chatRooms)) {
@@ -51,17 +59,39 @@ io.on('connection', (socket) => {
     socket.join(roomId);
     socket.roomId = roomId;// for sys, the room id is same as self socket id
 
+    // init system dialog
+    dialogInfo[socket.id] = {
+      "context": [],
+      "system_profile_id": socket.id,
+      "user_profile_id":''
+    };
+    
   });
 
   // allocate room
   socket.on('allocate room', (userInfo) => {
 
+    socket.userInfo = userInfo;
     var topic = userInfo['topic'];
+
     if (chatRooms[topic].length > 0) {
+      
+      // allocate based on topic
       var roomId = chatRooms[topic].pop();
       socket.join(roomId);
       socket.roomId = roomId;
+      
+      // init user dialog
+      dialogInfo[socket.id] = {
+        "context": [],
+        "system_profile_id": roomId,
+        "user_profile_id":socket.id
+      };
+      
+      // set system's user_profile_id is current socketid
+      dialogInfo[roomId]['user_profile_id'] = socket.id;
     } else {
+      // not available 
       console.log('no available room for topic: ', topic);
     }
   });
@@ -69,7 +99,7 @@ io.on('connection', (socket) => {
   // when the client emits 'add user', this listens and executes
   socket.on('add user', (username) => {
     if (addedUser) return;
-    // we store the username in the socket session for this client
+    // we store the username in the socket session for this client    
     socket.username = username;
     ++numUsers;
     addedUser = true;
@@ -101,6 +131,12 @@ io.on('connection', (socket) => {
   socket.on('disconnect', () => {
     if (addedUser) {
       --numUsers;
+      // call save api to backup dialog
+      if (dialogInfo[socket.id] !== undefined && dialogInfo[socket.id]['context'] !== undefined && dialogInfo[socket.id]['context'].length > 0) {
+        var context = dialogInfo[socket.id]['context'];
+        context[context.length - 1]['terminal'] = true;
+        console.log(dialogInfo[socket.id]);
+      }
 
       // echo globally that this client has left
       socket.broadcast.emit('user left', {
